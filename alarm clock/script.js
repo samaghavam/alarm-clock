996795b2aca2a95e84202f5a3f04b42fd8e1bc8a
sama.Ghavamzadeh <sghavamzadeh46@gmail.com>
2024-02-10 14:26:09 +0330

    add current time to h1 and refreshing every second

--- alarm clock/script.js
+++ alarm clock/script.js
@@ -1,6 +1,7 @@
-// adding options for hour and minutes
 const selectMenu = document.querySelectorAll('select');
+const timebox = document.querySelector('.time');
 
+// adding options for hour and minutes
 // for hours
 for(let i = 23 ; i >= 0 ; i--){
     i = i < 10 ? '0' + i : i;
@@ -15,6 +16,22 @@ for(let i = 59 ; i >= 0 ; i--){
     let option = `<option value="${i}">${i}</option>`;
     selectMenu[1].firstElementChild.insertAdjacentHTML('afterend', option);
 }
+
+setInterval(() => {
+    let date = new Date();
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+    
+    h = h < 10 ? '0' + h : h;
+    m = m < 10 ? '0' + m : m;
+    s = s < 10 ? '0' + s : s;
+
+    timebox.innerHTML = `${h} : ${m} : ${s}`;
+}, 1000);
+
+
+
 
 
 
